c795990afbb2ab25b31c4fe9a725e3d349490a68
Leonardo A. Alvarenga <leonardo.a.alvarenga@gmail.com>
2025-04-07 02:20:52 -0300

    feat: add new contact section

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,12 +1,20 @@
 export const CONTACTS = [
   {
     icon: "devicon-linkedin-plain",
+    isDevIcon: true,
     label: "LinkedIn",
     href: "https://linkedin.com/in/leonardo-a-alvarenga/",
   },
   {
     icon: "devicon-github-original",
+    isDevIcon: true,
     label: "GitHub",
     href: "http://github.com/leo-alvarenga",
+  },
+  {
+    download: "leonardo_alvarenga_cv.pdf",
+    icon: "fa6-solid:download",
+    label: "home.contact.cv.action",
+    href: "/documents/CV_{{language}}.pdf",
   },
 ];
